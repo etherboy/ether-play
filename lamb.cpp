a67cb393b4d517deaa4b888b5a27448444ecd79a
fvander <fvander@techlinkentertainment.com>
2013-10-01 13:14:07 -0300

    Used a lambda with std::sort and used a ranged for.

--- lamb.cpp
+++ lamb.cpp
@@ -2,6 +2,8 @@
 
 #include <string>
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 void LambdaFun()
 {
@@ -16,5 +18,13 @@ void LambdaFun()
 
 	//single reference capture, single parameter, notice order of evaluation
 	auto aLamb3 = [&aInt](int aMultiple) { aInt *= aMultiple; return aInt; };
-	std::cout << "Lamb3: " << aLamb3(3) << " again  " << aLamb3(6) << std::endl; 
+	std::cout << "Lamb3: " << aLamb3(3) << " again  " << aLamb3(6) << std::endl;
+
+	std::vector<int> aVec = { 24,5,875,43,2,89 };
+	std::sort(aVec.begin(), aVec.end(), [](int a1, int a2){ return a1 > a2; } );
+	for(int aValue : aVec)
+	{
+		std::cout << aValue << " ";
+	}
+	std::cout << std::endl; 
 }
