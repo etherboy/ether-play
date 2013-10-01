891b4b4e4a114dfd654e4dada84cec48826bdf4d
fvander <fvander@techlinkentertainment.com>
2013-10-01 09:30:24 -0300

    Added a few lambdas

--- lamb.cpp
+++ lamb.cpp
@@ -7,5 +7,14 @@ void LambdaFun()
 {
 	//no capture, one parameter
 	auto aLamb = [](const std::string& aValue){ return aValue + "ly"; };
-	std::cout << aLamb("heaven") << std::endl; 
+	std::cout << aLamb("heaven") << std::endl;
+
+	//single value capture, no parameter, mutable
+	int aInt = 100;
+	auto aLamb2 = [aInt]() mutable { aInt += 10; return aInt; };
+	std::cout << "Lamb2: " << aLamb2() << std::endl;
+
+	//single reference capture, single parameter, notice order of evaluation
+	auto aLamb3 = [&aInt](int aMultiple) { aInt *= aMultiple; return aInt; };
+	std::cout << "Lamb3: " << aLamb3(3) << " again  " << aLamb3(6) << std::endl; 
 }
